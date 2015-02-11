cf1b3addb0145e94aa1f998b6f2749889ba7fd1d
Ricardo Smania <ricardo.smania@mega.com.br>
2015-02-11 19:21:54 -0200

    Inicial

---
+++ Assets/Scripts/Hominho.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hominho : MonoBehaviour {
+	public GameObject hominho;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow)) 
+		  hominho.rigidbody2D.AddForce (new Vector2 (3.0f, 0.0f));
+		else if (Input.GetKey(KeyCode.LeftArrow)) 
+			hominho.rigidbody2D.AddForce (new Vector2 (-3.0f, 0.0f));
+		if (Input.GetKey (KeyCode.UpArrow))
+			hominho.rigidbody2D.AddForce (new Vector2 (0.0f, 10.0f));
+
+	}
+
+}
